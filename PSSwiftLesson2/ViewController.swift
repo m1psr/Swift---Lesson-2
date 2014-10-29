29d4770a7e159c016070879b0a07e3bb87e6c6d5
Mikhai1 <m@MacBook-Air-M.local>
2014-10-29 20:53:15 +0300

    готов слайдер

--- PSSwiftLesson2/ViewController.swift
+++ PSSwiftLesson2/ViewController.swift
@@ -9,10 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var scrollView:UIScrollView?
+    var timer:NSTimer? = nil
+    
+    let frameOffsets:[Int] = [45, 110, 130, 110, 95, 100, 100, 85, 90, 95, 105, 105, 100]
+    let frameWidth = 100 // in points
+    let frameHeight = 300 // in points
+    var currentSlide = 0
+    
+    // MARK: ViewController Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.grayColor()
+        
+        // scrollView
+        
+        let image:UIImage? = UIImage(named: "strip")
+    
+        let startYPos = CGFloat((self.view.bounds.size.height - image!.size.height) / 2.0)
+        
+        scrollView = UIScrollView(frame: CGRectMake(160, startYPos, CGFloat(frameWidth), CGFloat(frameHeight)))
+        self.view.addSubview(scrollView!)
+        
+        let imageView = UIImageView(image: image)
+        scrollView!.addSubview(imageView)
+        
+        // button Start
+        
+        let buttonYOffset:CGFloat = 50;
+        
+        var buttonStart:UIButton = UIButton(frame: CGRect(x: 40, y: startYPos + buttonYOffset, width: 100, height: 50))
+        buttonStart.setTitle("Авто старт", forState: UIControlState.Normal)
+        buttonStart.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Highlighted)
+        buttonStart.addTarget(self, action: "startTimer", forControlEvents: .TouchUpInside)
+        self.view.addSubview(buttonStart)
+        
+        // button Stop
+        
+        var buttonStop:UIButton = UIButton(frame: CGRect(x: 40, y: startYPos + buttonYOffset * 2, width: 100, height: 50))
+        buttonStop.setTitle("Авто стоп", forState: UIControlState.Normal)
+        buttonStop.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Highlighted)
+        buttonStop.addTarget(self, action: "stopTimer", forControlEvents: .TouchUpInside)
+        self.view.addSubview(buttonStop)
+        
+        // button Left
+        
+        var buttonLeft:UIButton = UIButton(frame: CGRect(x: 40, y: startYPos + buttonYOffset * 3, width: 100, height: 50))
+        buttonLeft.setTitle("Влево", forState: UIControlState.Normal)
+        buttonLeft.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Highlighted)
+        buttonLeft.addTarget(self, action: "leftButtonTapped", forControlEvents: .TouchUpInside)
+        self.view.addSubview(buttonLeft)
+        
+        // button Right
+        
+        var buttonRight:UIButton = UIButton(frame: CGRect(x: 40, y: startYPos + buttonYOffset * 4, width: 100, height: 50))
+        buttonRight.setTitle("Вправо", forState: UIControlState.Normal)
+        buttonRight.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Highlighted)
+        buttonRight.addTarget(self, action: "rightButtonTapped", forControlEvents: .TouchUpInside)
+        self.view.addSubview(buttonRight)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        sleep(1)
+        currentSlide = -1
+        nextSlide()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +85,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Slider Controls
+    
+    func showCurrentSlide() {
+        var offset = 0
+        for i in 0...currentSlide {
+            offset += frameOffsets[i]
+        }
+        scrollView!.setContentOffset(CGPoint(x: offset, y: 0), animated: true)
+    }
+    
+    func prevSlide() {
+        if (currentSlide > 0) {
+            currentSlide--
+            showCurrentSlide()
+        } else {
+            stopTimer()
+        }
+    }
+    
+    func nextSlide() {
+        if (currentSlide < frameOffsets.count - 1) {
+            currentSlide++
+            showCurrentSlide()
+        } else {
+            stopTimer()
+        }
+    }
+    
+    // MARK: Timer Controls
+    
+    func startTimer() {
+        if timer == nil {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector:Selector("nextSlide"), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func stopTimer() {
+        if let t = timer {
+            t.invalidate()
+            timer = nil
+        }
+    }
+    
+    // MARK: IBActions Methods
+    
+    func leftButtonTapped() {
+        stopTimer()
+        prevSlide()
+    }
+    
+    func rightButtonTapped() {
+        stopTimer()
+        nextSlide()
+    }
 }
-
